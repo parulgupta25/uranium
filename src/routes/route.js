ca7138ea8f6ee920adbe76c04ac84187a767d8a3
parulgupta25 <parulg1999@gmail.com>
2022-04-09 19:14:56 +0530

    post-request-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,70 +1,96 @@
 const express = require('express');
+const req = require('express/lib/request');
+const { route } = require('express/lib/application');
+const randomController = require('../controllers/randomController')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    // let a = { msg: "My first ever API response in JSON !!"} 
+router.get('/missingNum', function(req, res){
+    const arr = [1,2,4,5,6,7]
+    let total = 0
+    
+    for (let i in arr) {
+        const element = arr[i];
+        total += element
+    }
 
+    const lastDigit = arr.pop()
+    const consecutiveSum = lastDigit * (lastDigit+1) / 2
+    const missingNumber = consecutiveSum - total
 
-    res.send( { msg: "My first ever API response in JSON !!"} )
+    res.send({data: missingNumber})
 });
 
+router.get('/missNum2', function(req, res){
+    const arr = [33,34,35,37,38,39]
+    const len = arr.length
 
+    let total = 0
+    for (let i in arr) {
+        const element = arr[i];
+        total += element
+    }
 
-router.get('/test-api1', function (req, res) {
+    const firstDigit = arr[0]
+    const lastDigit = arr.pop()
+    const consecutiveSum = (len+1) *(firstDigit+lastDigit)/2
+    const missingNumber = consecutiveSum - total
 
-    res.send( "hi FunctionUp " )
+    res.send({data: missingNumber})
 });
 
+let players = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ],
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ],
+    },
+]
 
-router.get('/test-api2', function (req, res) {
+router.post('/players', function(req, res){
+    let x = req.body.name
+    let newPlayer = req.body
+    
+    let flag = 0
+    for (let index = 0; index < players.length; index++) {
+        let element = players[index];
+        let {name} = element
+        if(x === name){
+            flag = 1
+            break
+        }
+    }
+    if(flag === 0){
+        players = [...players, newPlayer]
+    }
 
-    res.send( { msg: "Hi FUnctionUp..again !"} )
+    res.send( {data: players, status: true} )
 });
 
-
-router.get('/test-api3', function (req, res) {
-
-    res.send( { msg: "Hi FUnctionUp..again..this is another similar api !"} )
-});
-
-
-router.get('/test-api4', function (req, res) {
-
-    res.send( { msg: "Hi FUnctionUp..again..this is another similar api ..not I am getting bored!"} )
-});
-
-
-router.get('/test-api5', function (req, res) {
-
-    res.send( { msg: "Hi FUnctionUp" , name:"FunctionUp", age: "100"} )
-});
-
-
-
-router.get('/test-api6', function (req, res) {
-
-    res.send( {   data: [12, 24, 36, 48, 60]  }   )
-});
-
-router.post('/test-post1', function (req, res) {
-
-    res.send( {  msg: "hi guys"  }   )
-});
-
-
-// to send data in  post request-> prefer sending in BODY -> click body-raw-json
-router.post('/test-post2', function (req, res) {
-    let data= req.body
-    console.log(data)
-    res.send( {  msg: "hi guys..my 2nd post req"  }   )
-});
-
-
-const randomController= require("../controllers/randomController.js")
-//write a post request to accept an element in post request body and add it to the given array and return the new array
-router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
-
+router.post("/test-post2", randomController.addToArray);
 
 
 module.exports = router;
+// adding this comment for no reason
