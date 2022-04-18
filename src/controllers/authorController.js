9dc465162ac5e589dd72e023cb59c031b42fc362
parulgupta25 <parulg1999@gmail.com>
2022-04-19 01:55:15 +0530

    pop-ref2-assign

--- src/controllers/authorController.js
+++
@@ -1,9 +0,0 @@
-const authorModel = require('../models/authorModel')
-
-const createNewAuthor = async function(req, res){
-    let newAuthor = req.body
-    let authorCreated = await authorModel.create(newAuthor)
-    res.send({data: authorCreated})
-}
-
-module.exports.createNewAuthor = createNewAuthor
--- src/models/publisherModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const publisherSchema = new mongoose.Schema({
-    name: String,
-    headQuarter: String
-},{timestamp : true});
-
-module.exports = mongoose.model('newPublisher', publisherSchema)
